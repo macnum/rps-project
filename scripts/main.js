689b21d91bc543e14a113869b60125e42117ef01
macnum <w.damilola@yahoo.com>
2024-03-03 16:13:55 +0100

    feat: Add a means of counting

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,11 @@ const selection = document.querySelector('.selection');
 const  player = document.getElementById("user");
 const  computer = document.getElementById("computer");
 const  wins = document.querySelector('#win')
-
+const played = document.querySelector("#played")
+const total = document.querySelector("#total")
+let count = 0;
+let userCount = 0;
+let computerCount = 0;
 
 function getRandomNUmber () {
 	return Math.floor(Math.random() * 3)
@@ -23,6 +27,25 @@ function getComputerChoice() {
 selection.addEventListener('click' ,(e) => {
 	let user = e.target.id;	
 	checkSelection(getComputerChoice(), user);
+	played.textContent = `Played ${count}`;
+	if(count === 5) {
+		
+		if(userCount === computerCount) {
+			total.innerHTML = `A Tie -try again &#12551;`;
+
+		}
+		else if (userCount > computerCount) {
+			total.innerHTML = `Who won: You won &#12951`;
+		}
+		else if (computerCount > userCount){
+			total.innerHTML = `Who won: Computer wins &#128546`;
+		}
+		count = 0;
+		userCount =0;
+		computerCount =0;
+		
+
+	}
 	
 })
 
@@ -35,15 +58,19 @@ function checkSelection(computerChoice , userChoice) {
 	computerChoice === "paper" && userChoice === "rock" ||
 	computerChoice === "scissors" && userChoice === "paper") {
 		wins.textContent = "Computer wins";
-		
+		count++;
+		computerCount++;		
 	}
 	else if(userChoice === "rock" && computerChoice === "scissors" || 
 		userChoice === "paper" && computerChoice === "rock" ||
 		userChoice === "scissors" && computerChoice === "paper") {
 			wins.textContent = "User wins";
+			count++;
+			userCount++
 		}
 	else {
 		wins.textContent = "This is a draw";
+		count++;
 	}	
 
 }
